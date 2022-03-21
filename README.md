4e158f34697057c3998913288928f1dec56877a1
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-03-21 11:33:21 -0300

    Criação dos Botões de tipo

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 ## Projeto
 
-O dt money é um sistema de gestão financeira desenvolvido durante o segundo módulo da trilha ReactJs do [Ignite](https://www.rocketseat.com.br/ignite), oferecido pela [Rocketseat](https://www.rocketseat.com.br).
+O dt money é um sistema de gestão financeira desenvolvido durante o segundo módulo da trilha ReactJS do [Ignite](https://www.rocketseat.com.br/ignite), oferecido pela [Rocketseat](https://www.rocketseat.com.br).
+
+## Fetuares
+
+- [x] Visão geral da situação financeira
+- [x] Cadastro e exclusão de transações
+<!-- - [x] Controle de contas a pagar e receber -->
 
 ## Techs
 
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+
+import { Container, TransactionTypeContainer } from './styles';
 
 interface newTransactionModalProps {
   isOpen: boolean;
@@ -26,6 +29,22 @@ export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionMo
 
         <input type="number" placeholder="Valor"
         />
+
+        <TransactionTypeContainer>
+          <button
+            type='button'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type='button'
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
